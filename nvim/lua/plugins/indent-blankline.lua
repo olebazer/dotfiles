18eaa8b7fc1c65b7eab4d9258e2727fa744f063f
olebazer <o.basermann@gmail.com>
2023-12-30 20:38:35 +0100

    added new plugin to nvim

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,25 @@
+-- indent blank line
+
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {},
+	config = function()
+        local highlight = {
+            "BoringHighlight",
+        }
+        local hooks = require("ibl.hooks")
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "BoringHighlight", { fg = "#242430" })
+        end)
+		require("ibl").setup({
+            indent = {
+                highlight = highlight,
+                --char = "|",
+            },
+            scope = {
+                enabled = false,
+            }
+		})
+	end,
+}
